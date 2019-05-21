05454a36a62299a131cc29202dc388580c552582
MiguelAngelMH <48890755+MiguelAngelMH@users.noreply.github.com>
2019-05-21 19:28:29 +0200

    Add files via upload

---
+++ TRABAJOS JAVA/caja/src/caja/caja1.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package caja;
+
+/**
+ *
+ * @author PC18-LB02
+ */
+public class caja1 {
+    int ancho;
+    int altura;
+    int profundo;
+    caja1 (){}
+    caja1(int ancho, int altura,int profundo){
+        this.ancho=ancho;
+        this.altura=altura;
+        this.profundo=profundo;
+    }
+    int volumen(){
+        return ancho*altura*profundo;
+    }
+}
